4e98e0472e4477493bb798035072a9738d6448f5
Ari Schermuk <arielschermuk@gmail.com>
2022-03-10 18:50:43 -0300

    Undo Solution

--- src/main/java/org/dojo/Sketch.java
+++ src/main/java/org/dojo/Sketch.java
@@ -11,53 +11,18 @@ public class Sketch extends PApplet {
 
     @Override
     public void settings() {
-        size(600, 600);
+        //size(w, h); //Set size of the canvas
     }
 
     @Override
     public void setup() {
         objects = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
-            objects.add(new Drop());
-        }
+        //Add objects to add
     }
 
     @Override
     public void draw() {
-        background(20, 20, 70);
+        //background(r, g, b); //Draw background on each iteration
         objects.forEach(ActableMovingObject::act);
     }
-
-    public class Drop extends ActableMovingObject {
-
-        public Drop() {
-            restartPhysics();
-        }
-
-        private void restartPhysics() {
-            this.x = random(width);
-            this.y = random(-200, -100);
-            this.z = random(1, 20);
-            this.ySpeed = map(z, 1, 20, 0.5F, 3);
-            this.yAccel = map(z, 1, 20, 0.2F, 1);
-            this.weight = map(z, 1, 20, 1, 3);
-            this.lenght = map(z, 1, 20, 7, 18);
-        }
-
-        private float weight, lenght;
-
-        public void move() {
-            super.move();
-            if (y > height) {
-                restartPhysics();
-            }
-        }
-
-        public void show() {
-            stroke(50, 50, 200);
-            strokeWeight(weight);
-            line(x, y, x, y - lenght);
-        }
-    }
-
 }
