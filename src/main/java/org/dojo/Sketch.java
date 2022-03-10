0d67097db0d0e3408b7a7ec00ea1780c144e5ba9
Ari Schermuk <arielschermuk@gmail.com>
2022-03-10 18:48:06 -0300

    Make Drop an inner Class

--- src/main/java/org/dojo/Sketch.java
+++ src/main/java/org/dojo/Sketch.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Sketch extends PApplet {
-    private List<Drop> drops;
+    private List<ActableMovingObject> objects;
 
     @Override
     public void settings() {
@@ -16,22 +16,48 @@ public class Sketch extends PApplet {
 
     @Override
     public void setup() {
-        drops = new ArrayList<>();
+        objects = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
-            drops.add(new Drop(this));
+            objects.add(new Drop());
         }
     }
 
     @Override
     public void draw() {
-        background(20,20,70);
-        drops.forEach(getDropConsumer());
+        background(20, 20, 70);
+        objects.forEach(ActableMovingObject::act);
     }
 
-    private Consumer<Drop> getDropConsumer() {
-        return d -> {
-            d.move();
-            d.show();
-        };
+    public class Drop extends ActableMovingObject {
+
+        public Drop() {
+            restartPhysics();
+        }
+
+        private void restartPhysics() {
+            this.x = random(width);
+            this.y = random(-200, -100);
+            this.z = random(1, 20);
+            this.ySpeed = map(z, 1, 20, 0.5F, 3);
+            this.yAccel = map(z, 1, 20, 0.2F, 1);
+            this.weight = map(z, 1, 20, 1, 3);
+            this.lenght = map(z, 1, 20, 7, 18);
+        }
+
+        private float weight, lenght;
+
+        public void move() {
+            super.move();
+            if (y > height) {
+                restartPhysics();
+            }
+        }
+
+        public void show() {
+            stroke(50, 50, 200);
+            strokeWeight(weight);
+            line(x, y, x, y - lenght);
+        }
     }
+
 }
